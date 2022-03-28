0fa2dc9aa026e14be4b8a66c104f65c278b5f379
Saad332556 <332556@student.mboutrecht.nl>
2022-03-28 09:28:06 +0200

    small stuff

--- index.php
+++ index.php
@@ -37,8 +37,8 @@
                 <a href="#gaming">Gaming</a>
                 <a href="#contact">Contact</a>
                 <div class="topnav-right">
-                    <a href="../registratie/login.php">Login</a>
-                    <a href="register.php">Abonneer nu</a>
+                    <a href="./login.php">Login</a>
+                    <a href="./register.php">Abonneer nu</a>
                 </div>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                 <i class="fa fa-bars"></i></a>
@@ -51,15 +51,15 @@
                 <h2>The latest news</h2>
                 <div class="mySlides fade">
                     <img src="./img/aot.jpeg" style="width:100%">
-                    <div class="text">Probably 2 more week for the end of the famous anime Attack On Titan</div>
+                    <div class="text">Famous anime Attack On Titan, about to end?</div>
                 </div>
                 <div class="mySlides fade">
                     <img src="./img/75 nba.png" style="width:100%">
                     <div class="text">NBA 75th Anniversary</div>
                 </div>
                 <div class="mySlides fade">
-                    <img src="./img/EldenRing.jpg" style="width:100%">
-                    <div class="text">New game called Elden Ring out now!</div>
+                    <img src="./img/CallOfDuty_SnoopDogg.jpg" style="width:100%">
+                    <div class="text">Snoop Dogg coming to CoD??</div>
                 </div>
             </div>
             <br>
@@ -72,30 +72,41 @@
                 <div class="flexbox">
                     <div class="top3news1">
                         <img src="./img/aots4p2.jpeg" alt="">
-                        <h2 class="bask_titel1"><b>Attack on Titan</b></h2>
+                        <h2 class="titel"><b>Attack on Titan</b></h2>
                         <p class="top3_text1">
-                            The well-known anime (not to be confused with manga!) called Attack on Titan, AKA AoT, is soon to reach it's end sadly.
-                            The anime started on April 7th 2013.
+                            The well-known anime (not to be confused with manga!) called Attack on Titan, AKA AoT, is soon to reach
+                            it's end sadly. The anime started on April 7th 2013.
                         </p>
+
                     </div>
                     <div class="top3news2">
-
-                        <img src="./img/steph.jpg" alt="">
+                    <img src="./img/NBA-75.jpg" alt="">
+                        <h2 class="titel"><b>NBA's 75th anniversary</b></h2>
                         <p class="top3_text2">
-                            After Golden State Warriors guard Stephen Curry tumbled to the floor, Boston Celtics guard Marcus Smart inadvertently dove toward his knees. Will the Warriors’ NBA title chances also dive because of that sequence?
-
-                            The Warriors said they will reevaluate Curry in two weeks after an MRI revealed that he sprained a ligament in his left foot ligament following Smart’s collision in last Wednesday’s loss to Boston. That ensures that Curry will have missed seven contests, which includes Sunday’s loss to San Antonio as well as games against Orlando (Tuesday), Miami (Wednesday), Atlanta (Friday), Washington (Sunday), Memphis (March 28) and Phoenix (March 30).
-
-                            But what about after that? Will Curry play in any regular-season games? How about the playoffs?
-
-                            There are lots of unanswered questions about him and it’s no wonder, then, that a few Curry-related questions have appeared in this week’s mailbag.
+                            NBA All-Star Weekend was a showcase of today's superstars, but it was also a celebration of the best
+                            players throughout 75 years of league history.
                         </p>
+
+                        <p class="top3_text2">
+                            Earlier in the 2021-22 season, the NBA unveiled its 75th Anniversary Team, a squad full of iconic players
+                            representing different points in the evolution of basketball.
+                            ESPN's NBA expert panel voted on thousands of head-to-head player matchups, taking into consideration the
+                            quality and quantity of their NBA contributions.
+                        </p>
+
                     </div>
                     <div class="top3news3">
                         <img src="./img/warzone.jpeg" alt="">
+                        <h2 class="titel"><b>Attack on Titan</b></h2>
                         <p class="top3_text3">
-                            Spelers die met Snoop Dogg aan de slag willen Call of Duty Warzone en Vanguard moeten iets langer wachten. Zij kunnen namelijk met de nieuwe Operator aan de slag vanaf 19 april. Daarvoor zal je de Snoop Dogg Operator Bundle moeten aanschaffen, waarmee je toegang krijgt tot een volledige Operator progression track. Naarmate je stijgt in level zal je tien verschillende items ontgrendelen, waarbij er drie exclusief zijn voor Call of Duty Vanguard.
-                            Het is niet de eerste keer dat Snoop Dogg in een Call of Duty game verschijnt. Hij was eerder al te horen in Call of Duty Ghosts via een voiceover pack. Deze keer zal hij dus niet alleen zijn stem, maar ook zijn gelijkenis verlenen aan de game.
+                            Players who want to get started with Snoop Dogg in Call of Duty Warzone and Vanguard will have to wait a
+                            little longer. They can start working with the new Operator from 19 April. For that you will need to
+                            purchase the Snoop Dogg Operator Bundle, which gives you access to a full Operator progression track. 
+                            As you level up you will unlock ten different items, three of which are exclusive to Call of Duty Vanguard.
+
+                            It's not the first time Snoop Dogg has appeared in a Call of Duty game. He was previously heard in Call of
+                            Duty Ghosts via a voiceover pack. So this time he will not only lend his voice, but also his likeness to
+                            the game.
                         </p>
 
                     </div>
@@ -109,28 +120,30 @@
             <div class="flexbox">
                 <div class="basketball1">
                     <img src="./img/56points.jpg" class="responsive">
-                    <h2 class="bask_titel1"><b>LeBron James 56 points</b></h2>
+                    <h2 class="titel"><b>LeBron James 56 points</b></h2>
                     <p class="bask_text1"> 
-                        In much more significant games that decided his NBA championship fortunes, LeBron James basked in his greatness by bathing in
-                        champagne. This time, James’ teammates dumped water on him
+                        In much more significant games that decided his NBA championship fortunes, LeBron James basked in his greatness
+                        by bathing in
+                        champagne. This time, James’ teammates dumped water on him.
                     </p>
 
                     <p class="bask_text1">
-                        James ensured the Lakers escaped with a 124-116 win over the Golden State Warriors on Saturday by fulfilling a job description
-                        few in NBA history have done before. 
+                        James ensured the Lakers escaped with a 124-116 win over the Golden State Warriors on Saturday by fulfilling a
+                        job description few in NBA history have done before. 
                     </p>
                     <p class="bask_text1">    
                         By finishing with a season-high 56 points while shooting 19-for-31 from the field including six from
-                        3-point range and nearly perfect (12-for-13) from the free-throw line along with 10 rebounds, the 37-year-old James became the oldest player in
-                        NBA history to post 50-plus points and 10-plus rebounds in a game. He also became the fourth-oldest player to post a 50-point game.
+                        3-point range and nearly perfect (12-for-13) from the free-throw line along with 10 rebounds, the 37-year-old
+                        James became the oldest player in NBA history to post 50-plus points and 10-plus rebounds in a game. He also
+                        became the fourth-oldest player to post a 50-point game.
                     </p>
                 </div>
                 <div class="basketball2">
                     <img src="./img/smh.jpg" class="responsive">
-                    <h2 class="bask_titel2"><b> Simmons & Harden Trade</b></h2>
+                    <h2 class="titel"><b> Simmons & Harden Trade</b></h2>
                     <p class ="bask_text2">
-                        The Brooklyn Nets and Philadelphia 76ers have completed a blockbuster trade centered around former Kia NBA MVP James Harden and former All-Star
-                        guard Ben Simmons, according to multiple reports.
+                        The Brooklyn Nets and Philadelphia 76ers have completed a blockbuster trade centered around former Kia NBA MVP
+                        James Harden and former All-Star guard Ben Simmons, according to multiple reports.
                     </p>
 
                     <p class="bask_text2p2"><b>Brooklyn</b> receives:</p>
@@ -153,15 +166,17 @@
                 </div>
                 <div class="basketball3">
                     <img src="./img/westbrook.jpg" class="responsive">
-                    <h2 class="bask_titel3"><b> Russell Westbrook trade</b></h2>
+                    <h2 class="titel"><b> Russell Westbrook trade</b></h2>
                     <p class = "bask_text3">
-                        The NBA trade deadline came and went this past Thursday. The prospect of the Lakers shaking up their roster went with it. Plenty of reports have
-                        since surfaced of what few trade options the Lakers had with their limited assets. That included, confirmation that the much discussed Russell
-                        Westbrook for John Wall trade was still on the table for the LA.
+                        The NBA trade deadline came and went this past Thursday. The prospect of the Lakers shaking up their roster
+                        went with it. Plenty of reports have since surfaced of what few trade options the Lakers had with their limited
+                        assets. That included, confirmation that the much discussed Russell Westbrook for John Wall trade was still on
+                        the table for the LA.
                     </p>
                       
                     <p class = "bask_text3">
-                        It was previously reported that Houston wanted the Lakers 2027 first-round pick in exchange for taking on Westbrook's contract.
+                        It was previously reported that Houston wanted the Lakers 2027 first-round pick in exchange for taking on
+                        Westbrook's contract.
                     </p>
                 </div>
             </div>
@@ -169,7 +184,7 @@
             <div class="flexbox">
                 <div class="gaming1">
                     <img src="./img/gta 6.jpg">
-                    <h2 class="game_titel1"><b>GTA 6 Leak</b></h2>
+                    <h2 class="titel"><b>GTA 6 Leak</b></h2>
                     <p class="game_text1">
                         It's almost a shock that GTA 6 is officially happening. Earlier this year, developer Rockstar
                         Games confirmed that "active development for the next entry in the Grand Theft Auto series is well underway."
@@ -184,8 +199,8 @@
                 </div>
                 <div class="gaming2">
                     <img src="./img/Ps-Plus-.jpg" alt="">
-                    <h2 class="game_titel1"><b> PS Plus Games April</b></h2>
-                    <p class ="game_text2">ach and every month, as the midway shifts toward the end, many fans become excited for the
+                    <h2 class="titel"><b> PS Plus Games April</b></h2>
+                    <p class ="game_text2">As the midway shifts toward the end, many fans become excited for the
                         next PS Plus free game reveal. No two months are created equal, however, and many haven’t seemed too happy with
                         PS Plus so far this year—except for March. PS Plus’ free games for February 2022 were exceptionably
                         controversial, giving out niche or not-full games.
@@ -199,7 +214,7 @@
                 </div>
                 <div class="gaming3">
                     <img src="./img/2k23.png" alt="">
-                    <h2 class="game_titel1"><b>2k23 Leaks</b></h2>
+                    <h2 class="titel"><b>2k23 Leaks</b></h2>
                     <p class = "game_text3">NBA 2K23 may still be months away, but even the continued success of NBA 2K22 isn't going
                         to slow down the conversation about what's coming next.
                     </p>
